bf042f50ed930c2cb5ca99c5b5d169446bba3a9f
Rithesh <kunchakuririthesh123@gmail.com>
2021-04-26 16:59:30 +0530

    Basic outcome commit

--- create.py
+++ create.py
@@ -1,14 +1,17 @@
-import sys
+#import sys
 import os
 from selenium import webdriver
 
-Path = r"C:\Users\Rithesh kunchakuri\Github\Myprojects"
+print("Enter new Github repository name ")
+FolderName = input()
+Parentpath = r"C:\Users\Rithesh kunchakuri\Github\Myprojects"
 browser = webdriver.Chrome(executable_path=r"C:\Users\Rithesh kunchakuri\Github\chromedriver.exe")
 browser.get("https://github.com/login")
 
-def create():
-    folderName = str(sys.argv[1])
-    os.makedirs(Path + str(sys.argv[1]))
+
+if __name__ == "__main__":
+    #Path = os.path.join(Parentpath , FolderName)
+    #os.makedirs(Path)
     python_button = browser.find_elements_by_xpath("/html/body/div[3]/main/div/div[4]/form/input[2]")[0]
     python_button.send_keys('rithesh6')
     python_button = browser.find_elements_by_xpath("/html/body/div[3]/main/div/div[4]/form/div/input[1]")[0]
@@ -17,7 +20,8 @@ def create():
     python_button.click()
     browser.get('https://github.com/new')
     python_button = browser.find_elements_by_xpath("/html/body/div[4]/main/div/form/div[2]/auto-check/dl/dd/input")[0]
-    python_button.send_keys(folderName)
-
-if __name__ == "__main__":
-    create()    
+    python_button.send_keys(FolderName)
+    python_button = browser.find_elements_by_xpath("/html/body/div[4]/main/div/form/div[4]/div[4]/div[1]/label/input[2]")[0]
+    python_button.click()  
+    python_button = browser.find_elements_by_css_selector('button.first-in-line')[0]
+    python_button.submit()  
--- mycommand.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-
-function create
-{
-    cd
-    python create.py "$1"
-    cd \Users\Rithesh kunchakuri\Github\Myprojects
-}
